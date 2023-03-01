09e7c3e5565a66e5c2d6733e10cec34b50569e64
Brandon Park <brandonmpark@gmail.com>
2023-03-01 11:39:02 -0800

    edit

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,21 +1,49 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class IsMoon implements StringChecker {
-  public boolean checkString(String s) {
-    return s.equalsIgnoreCase("moon");
-  }
+class TrueStringChecker implements StringChecker {
+    public boolean checkString(String s) {
+        return true;
+    }
+}
+
+class LengthStringChecker implements StringChecker {
+    public boolean checkString(String s) { return s.length() > 2;}
 }
 
 public class TestListExamples {
-  @Test(timeout = 500)
-  public void testMergeRightEnd() {
-    List<String> left = Arrays.asList("a", "b", "c");
-    List<String> right = Arrays.asList("a", "d");
-    List<String> merged = ListExamples.merge(left, right);
-    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
-    assertEquals(expected, merged);
-  }
+    @Test
+    public void testFilter() {
+        List<String> l1 = new ArrayList<>(Arrays.asList("a", "b", "c"));
+        assertArrayEquals(l1.toArray(), ListExamples.filter(l1,
+                new TrueStringChecker()).toArray());
+    }
+
+    @Test
+    public void testFilter2() {
+        List<String> l1 = new ArrayList<>(Arrays.asList("a", "ab", "abc",
+                "abcd"));
+        assertArrayEquals(new String[]{"abc", "abcd"}, ListExamples.filter(l1,
+                new LengthStringChecker()).toArray());
+    }
+
+    @Test
+    public void testMerge1() {
+        List<String> l1 = new ArrayList<>(Arrays.asList("x", "y"));
+        List<String> l2 = new ArrayList<>(Arrays.asList("a", "b"));
+        assertArrayEquals(new String[]{"a", "b", "x", "y"},
+                ListExamples.merge(l1, l2).toArray());
+    }
+
+    @Test
+    public void testMerge2() {
+        List<String> l1 = new ArrayList<>(Arrays.asList("a", "b", "c"));
+        List<String> l2 = new ArrayList<>(Arrays.asList("c", "d", "e"));
+        assertArrayEquals(new String[]{ "a", "b", "c", "c", "d", "e" },
+                ListExamples.merge(l1, l2).toArray());
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,5 +1,26 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-rm -rf student-submission
-git clone $1 student-submission
+rm -rf student-submission *.class ListExamples.java testOutput.txt
+echo 'Cloning...'
+git clone $1 student-submission &> testOutput.txt
+if ! [[ $? -eq 0 ]]
+then
+	echo 'Error cloning repository.'
+	exit
+fi
 echo 'Finished cloning'
+if ! [[ -f student-submission/ListExamples.java ]]
+then
+	echo 'Missing ListExamples.java file.'
+	exit
+fi
+cp student-submission/ListExamples.java .
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+if ! [[ $? -eq 0 ]]
+then
+	echo 'Compilation error.'
+	exit
+fi
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+# output=`cat testOutput.txt | grep '\.\(\.\|E\)*' -x`
+# echo $output
